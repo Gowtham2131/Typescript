f6897363bf7651749de1ba0f70c5ea817f0fd03e
Gowtham2131 <pvgowtham9677@gmail.com>
2026-09-11 22:42:02 +0530

    Added DataTypes

---
+++ DataType.js
@@ -0,0 +1,14 @@
+"use strict";
+var firstnumber = 100; // number variable 
+var Name = "Gowtham"; // String variable
+var b = true; //boolen variable
+console.log(firstnumber);
+//void 
+function samples() {
+    console.log("Hello");
+}
+samples();
+//Null
+var num1 = null;
+num1 = 100;
+console.log(num1);
---
+++ DataType.ts
@@ -0,0 +1,24 @@
+var firstnumber:number =100; // number variable 
+var Name:string ="Gowtham"// String variable
+var b:boolean= true //boolen variable
+console.log(firstnumber)
+
+//void 
+
+function samples():void
+{
+    console.log("Hello")
+}
+samples();
+
+//Null
+
+// var num1:number=undefined
+// num1=100
+// console.log(num1)
+
+// Any Variable
+
+var value :any ="ksjdsk"
+console.log(value)
+
